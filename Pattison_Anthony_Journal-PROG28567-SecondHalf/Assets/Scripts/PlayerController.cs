975c3b5f19125e49f728873f3e85d5a73728ecf8
valor87 <134859690+valor87@users.noreply.github.com>
2025-11-15 17:57:31 -0500

    finishing the journal and saving the scene. movement jumping and ground check is done

--- Pattison_Anthony_Journal-PROG28567-SecondHalf/Assets/Scripts/PlayerController.cs
+++ Pattison_Anthony_Journal-PROG28567-SecondHalf/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Movement")]
+    [SerializeField] float MovementSpeed;
+    [SerializeField] float JumpForce = 10;
+    [SerializeField] Rigidbody2D RB;
+    float Hinput;
+    float preFacingDirection;
+    [SerializeField] bool jump;
+    [SerializeField] float GroundCheck;
     public enum FacingDirection
     {
         left, right
@@ -9,34 +17,69 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        jump = false;
+        Hinput = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jump = true;
+        }
+
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
-        MovementUpdate(playerInput);
-    }
 
-    private void MovementUpdate(Vector2 playerInput)
+    }
+    private void FixedUpdate()
     {
 
+        Vector2 playerInput = new Vector2(Hinput, 0);
+        MovementUpdate(playerInput, MovementSpeed, jump);
+    }
+    private void MovementUpdate(Vector2 playerInput, float Speed, bool Jump)
+    {
+        Vector2 Playerpos = RB.position;
+        Playerpos += playerInput * Speed * Time.deltaTime;
+        RB.position = Playerpos;
+        if (!Jump || !IsGrounded())
+        {
+            return;
+        }
+
+        RB.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
     }
 
     public bool IsWalking()
     {
-        return false;
+        if (Hinput == 0)
+        {
+            return false;
+        }
+        preFacingDirection = Hinput;
+        return true;
     }
     public bool IsGrounded()
     {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + (new Vector3(0, -.65f, 0)), Vector2.down, .5f);
+        Debug.DrawRay(transform.position + (new Vector3(0, -.65f, 0)), Vector3.down * .5f);
+        if (hit.collider  != null && hit.collider.tag != "Player")
+        {
+            return true;
+        }
         return false;
     }
 
     public FacingDirection GetFacingDirection()
     {
+        if (preFacingDirection >= 0)
+        {
+            return FacingDirection.right;
+        }
         return FacingDirection.left;
+
     }
 }
